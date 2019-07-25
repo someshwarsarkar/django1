c9da5f32b64720c337daa91b3636c208ba567f0f
Someshwar Sarkar <someshwarsarkar9851@gmail.com>
2019-07-25 17:54:14 +0530

    demo

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -16,11 +16,17 @@ import hello.views
 
 urlpatterns = [
     path("", hello.views.index, name="index"),
-    path("about/", hello.views.about, name="about"),
-    path("notification/", hello.views.notification, name="notification"),
-    path("contact/", hello.views.contact, name="contact"),
-	path("add_product/", hello.views.add_product, name="add_product"),
-	path("insproduct/", hello.views.insproduct, name="insproduct"),
+    path("import_product/", hello.views.p_import, name="p_import"),
+    path("add_product/", hello.views.p_add, name="p_add"),
+    path("manage_product/", hello.views.p_manage, name="p_manage"),
+	path("add_category/", hello.views.c_add, name="c_add"),
+	path("manage_category/", hello.views.c_manage, name="c_manage"),
+	path("add_warehouse/", hello.views.w_add, name="w_add"),
+	path("manage_warehouse/", hello.views.w_manage, name="w_manage"),
+	path("export_product/", hello.views.export, name="export"),
+	path("invoice/", hello.views.invoice, name="invoice"),
+	path("report/", hello.views.report, name="report"),
+	path("contact_us/", hello.views.contact_us, name="contact_us"),
     path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
 ]
--- hello/views.py
+++ hello/views.py
@@ -7,29 +7,34 @@ from .models import Greeting
 def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, "index.html")
-
-def about(request):
-	n=request.POST.get("name")
-	e=request.POST.get("email")
-    # return HttpResponse('Hello from Python!')
-	return render(request, "about.html")
-
-def notification(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "notification.html")
-
-def contact(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "contact.html")
-def add_product(request):
-	return render(request, "add_product.html")
-def insproduct(request):
-	name=request.POST.get("name")
-	price=request.POST.get("price")
-	quantity=request.POST.get("quantity")
-	p_date=request.POST.get("p_date")
-	return render(request, "insproduct.html",{"name":name, "price":price, "quantity":quantity, "p_date":p_date})
-
+def p_import(request):
+    return render(request, "import.html")
+def p_add(request):
+    return render(request, "p_add.html")
+def p_manage(request):
+    return render(request, "p_manage.html")
+def c_add(request):
+    return render(request, "c_manage.html")
+def c_manage(request):
+    return render(request, "c_manage.html")
+def w_add(request):
+    return(request,"w_add.html")
+def w_manage(request):
+    return render(request, "w_manage.html")
+def export(request):
+    return render(request, "export.html")
+def invoice(request):
+    return render(request, "invoice.html")
+def report(request):
+    return render(request, "report.html")
+def contact_us(request):
+    return render(request, "contact_us.html")
+def front(request):
+    return render(request, "front.html")
+def navbar(request):
+    return render(request, "navbar.html")
+def sidebar(request):
+    return render(request, "sidebar.html")
 def db(request):
 
     greeting = Greeting()
