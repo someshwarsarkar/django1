474713154483c179c0ccbffc56449d960621eb93
Someshwar Sarkar <someshwarsarkar9851@gmail.com>
2019-07-27 19:18:02 +0530

    demo

--- hello/static/index.js
+++ hello/static/index.js
@@ -4,19 +4,12 @@ function openNav() {
   document.getElementById("main").style.marginLeft = "250px";
 }
 
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
-}
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
 }*/
+function closeNav() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("main").style.display = "none";
+}
