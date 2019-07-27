39e2755267abb95f9529008223bf1d464e2c60ff
Someshwar Sarkar <someshwarsarkar9851@gmail.com>
2019-07-27 20:58:39 +0530

    demo

--- hello/static/index.js
+++ hello/static/index.js
@@ -28,13 +28,13 @@ function openNav() {
      menuState = 1;
      document.getElementById("mySidebar").style.width = "250px";
      //document.getElementById("main").style.marginLeft = "250px";
-     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+     //document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
   else {
      menuState = 0;
      document.getElementById("mySidenav").style.width = "0";
      //document.getElementById("main").style.marginLeft = "0";
-     document.body.style.backgroundColor = "white";
+     //document.body.style.backgroundColor = "white";
   }
   console.log(menuState);
 } 
