644754b1e4ff258dab26ed7d729118003797b2b4
Someshwar Sarkar <someshwarsarkar9851@gmail.com>
2019-07-27 20:56:44 +0530

    demo

--- hello/static/index.js
+++ hello/static/index.js
@@ -20,22 +20,21 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }*/
-document.getElementById("myBtn").addEventListener("click", toggleNav);
+document.getElementById("myBtn").addEventListener("click", openNav);
 
-function toggleNav(){
-    navSize = document.getElementById("mySidenav").style.width;
-    if (navSize == 250) {
-        return close();
-    }
-    return open();
-}
-function open() {
-        document.getElementById("mySidenav").style.width = "250px";
-        document.getElementById("main").style.marginLeft = "250px";
-        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
-function close() {
-         document.getElementById("mySidenav").style.width = "0";
-        document.getElementById("main").style.marginLeft = "0";
-        document.body.style.backgroundColor = "white";
-}
+var menuState = 0 // close
+function openNav() {
+  if(menuState === 0){
+     menuState = 1;
+     document.getElementById("mySidebar").style.width = "250px";
+     //document.getElementById("main").style.marginLeft = "250px";
+     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  else {
+     menuState = 0;
+     document.getElementById("mySidenav").style.width = "0";
+     //document.getElementById("main").style.marginLeft = "0";
+     document.body.style.backgroundColor = "white";
+  }
+  console.log(menuState);
+} 
