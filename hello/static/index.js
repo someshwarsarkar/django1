423d6d3bfc908708e6ab31c8ea17e389e4179aca
Someshwar Sarkar <someshwarsarkar9851@gmail.com>
2019-07-27 20:46:47 +0530

    demo

--- hello/static/index.js
+++ hello/static/index.js
@@ -1,11 +1,11 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openNav() {
+/*function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
+/*function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
 }
@@ -20,3 +20,22 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }*/
+document.getElementById("myBtn").addEventListener("click", toggleNav);
+
+function toggleNav(){
+    navSize = document.getElementById("mySidenav").style.width;
+    if (navSize == 250) {
+        return close();
+    }
+    return open();
+}
+function open() {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+function close() {
+         document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+        document.body.style.backgroundColor = "white";
+}
