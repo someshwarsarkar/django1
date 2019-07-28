abb7bccb9dd662df07196632b21b0733d3fae17b
Someshwar Sarkar <someshwarsarkar9851@gmail.com>
2019-07-28 23:22:58 +0530

    demo

--- hello/static/index.js
+++ hello/static/index.js
@@ -10,29 +10,20 @@ btn.addEventListener("click",fur);
 
 function fur() {
   
-  if(sidebar.style.width == "0px"){
+  if(sidebar.style.width == "0px")
+	{
 
     sidebar.style.width = "250px";
     main.style.marginLeft = "250px";
 
-  }
+	}
    
-  else {
+  else 
+	{
   
-  sidebar.style.width = "0";
-  main.style.marginLeft = "0";
+  sidebar.style.width = "20px";
+  main.style.marginLeft = "20px";
   
+	}
 }
 }
-}
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-}*/
