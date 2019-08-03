8a9010d393ab7bac92b4da6dc4daf5055a1e88d7
Someshwar Sarkar <someshwarsarkar9851@gmail.com>
2019-08-03 23:42:10 +0530

    k

--- hello/views.py
+++ hello/views.py
@@ -111,7 +111,7 @@ def cat_delete(request):
 def print(request):
     i_id=request.POST.get("i_id")
     psel=Invoice.objects.filter(id=i_id)
-    return render(request, "print.html",{"i_id":i_id, "pname":psel.pname})
+    return render(request, "print.html",{"i_id":i_id})
 def db(request):
 
     greeting = Greeting()
